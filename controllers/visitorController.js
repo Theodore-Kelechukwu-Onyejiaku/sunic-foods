c12662bcaffd166fb3d9c34c83daedc439ffde33
Theodore-Kelechukwu-Onyejiaku <theodoreonyejiaku@gmail.com>
2020-03-27 21:09:11 +0100

    add visitor controller

---
+++ controllers/visitorController.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const router = express.Router();
+//const Menu = mongoose.model("menu");
+
+
+router.get("/", (req, res)=>{
+    res.render("visitorLayouts/index", {message: "Visitor Layout"});
+})
+
+router.get("/about", (req, res)=>{
+    res.render("visitorLayouts/about", {message: "Visitor Layout"});
+})
+
+router.get("/menu", (req, res)=>{
+    res.render("visitorLayouts/menu", {message: "Visitor Layout"});
+})
+
+router.get("/contact", (req, res)=>{
+    res.render("visitorLayouts/contact", {message: "Visitor Layout"});
+})
+
+
+module.exports = router;
